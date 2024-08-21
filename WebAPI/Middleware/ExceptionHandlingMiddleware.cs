1018f5aaabc4c2cb5fb3c3e9e815601ba706d374
wona825 <wona825@naver.com>
2024-08-22 06:20:28 +0900

    feat: add Exception Handler Middleware

---
+++ WebAPI/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Infrasfructure.Error;
+using System.Net;
+
+namespace WebAPI.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (CustomException ex)
+            {
+                await HandleExceptionAsync(context, ex.StatusCode, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, HttpStatusCode.InternalServerError, "An unexpected error occurred.");
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, HttpStatusCode statusCode, string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+            var result = System.Text.Json.JsonSerializer.Serialize(new { error = message });
+            return context.Response.WriteAsync(result);
+        }
+    }
+
+}
